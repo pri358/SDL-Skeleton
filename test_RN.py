7079db264be7edb0f6b3285b9ccac17c32ca56d2
Priyanshi Jain <priyanshi.jain030@gmail.com>
2021-01-05 14:48:37 +0530

    Update test_RN.py

--- test_RN.py
+++ test_RN.py
@@ -13,17 +13,19 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 import scipy.io as scio
+from google.colab.patches import cv2_imshow
+import cv2
 
 gpu_id = 0
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 torch.cuda.set_device(gpu_id)
-net = Network(128, 4, [0, 1, 2, 3], geno).cuda(0).eval()
-net.load_state_dict(torch.load('./Ada_LSN/weights/inception_sklarge/skel_74000.pth', map_location=lambda storage, loc: storage))
+net = Network(64, 5, [0, 1, 2, 3], geno).cuda(0).eval()
+net.load_state_dict(torch.load('/content/drive/MyDrive/IP - 7th sem/SK-SMALL/ADA_LSN/weights/skel_11000.pth', map_location=lambda storage, loc: storage))
 
-root = './OriginalSKLARGE/images/test'
-files = './OriginalSKLARGE/test.lst'
+root = '/content/drive/MyDrive/IP - 7th sem/SK-SMALL/SK506/images/test'
+# files = './OriginalSKLARGE/test.lst'
 
-dataset = TestDataset(root, files)
+dataset = TestDataset(root)
 dataloader = list(DataLoader(dataset, batch_size=1))
 
 
@@ -31,9 +33,11 @@ def plot_single_scale(scale_lst, size):
     pylab.rcParams['figure.figsize'] = size, size / 2
     plt.figure()
     for i, image in enumerate(scale_lst):
-        image = image.data[0, 0].cpu().numpy().astype(np.float32)
+        image = image[0,0].cpu().detach().numpy().astype(np.float32)
+        cv2_imshow(image)
         s = plt.subplot(1, 5, i + 1)
         plt.imshow(1 - image, cmap=cm.Greys_r)
+        cv2_imshow(1-image)
         s.set_xticklabels([])
         s.set_yticklabels([])
         s.yaxis.set_ticks_position('none')
@@ -41,15 +45,17 @@ def plot_single_scale(scale_lst, size):
     plt.tight_layout()
 
 
-idx = 16
+idx = 5
 inp, fname, H, W = dataloader[idx]
+# print(inp.shape, fname, H, W)
 inp = Variable(inp.cuda(gpu_id))
-out = net(inp)
+# print(inp)
+out, loss = net(inp)
 scale_lst = [out]
 plot_single_scale(scale_lst, 22)
 plt.show()
 
-output_dir = './Ada_LSN/output/inception_sklarge/'
+output_dir = '/content/drive/MyDrive/IP - 7th sem/SK-SMALL/ADA_LSN/output_images/'
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
@@ -57,11 +63,12 @@ start_time = time.time()
 tep = 1
 for inp, fname, H, W in dataloader:
     inp = Variable(inp.cuda(gpu_id))
-    out = net(inp)
+    out,loss = net(inp)
     fileName = output_dir + fname[0] + '.mat'
     tep += 1
-    out_np = out.data[0, 0].cpu().numpy()
+    out_np = out[0, 0].cpu().detach().numpy()
     out_resize = cv2.resize(out_np, (W.item(), H.item()), interpolation=cv2.INTER_LINEAR)
+    # cv2.imwrite(fileName, out_resize)
     scio.savemat(fileName, {'sym': out_resize})
     print('{} size{} resize{}'.format(fileName, out.size(2) * out.size(3), out_resize.shape[0] * out_resize.shape[1]))
 diff_time = time.time() - start_time
