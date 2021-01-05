e29815b32081200ac7c8dc7827c300539f1d2153
Priyanshi Jain <priyanshi.jain030@gmail.com>
2021-01-05 15:01:35 +0530

    Update sklarge.py

--- datasets/sklarge.py
+++ datasets/sklarge.py
@@ -10,14 +10,14 @@ class TrainDataset(Dataset):
     def __init__(self, fileNames, rootDir, transform=None):
         self.rootDir = rootDir
         self.transform = transform
-        self.frame = pd.read_csv(fileNames, dtype=str, delimiter=' ', header=None)
+        self.frame = pd.read_csv(fileNames, dtype=str, delimiter=',', header=None)
 
     def __len__(self):
         return len(self.frame)
 
     def __getitem__(self, idx):
-        inputName = os.path.join(self.rootDir, self.frame.iloc[idx, 0])
-        targetName = os.path.join(self.rootDir, self.frame.iloc[idx, 1])
+        inputName = self.frame.iloc[idx, 0]
+        targetName = self.frame.iloc[idx, 1]
 
         inputImage = io.imread(inputName)
         if len(inputImage.shape) == 2:
@@ -46,17 +46,18 @@ class TrainDataset(Dataset):
 
 
 class TestDataset(Dataset):
-    def __init__(self, fileNames, rootDir, transform=None):
+    def __init__(self, rootDir, transform=None):
         self.rootDir = rootDir
         self.transform = transform
-        self.frame = pd.read_csv(fileNames, dtype=str, delimiter=' ', header=None)
+        self.frame = sorted(os.listdir(rootDir))
+        # self.frame = pd.read_csv(fileNames, dtype=str, delimiter=' ', header=None)
 
     def __len__(self):
         return len(self.frame)
 
     def __getitem__(self, idx):
-        fname = self.frame.iloc[idx, 0]
-        inputName = os.path.join(self.rootDir, fname + '.jpg')
+        fname = self.frame[idx]
+        inputName = os.path.join(self.rootDir, fname)
 
         inputImage = io.imread(inputName)[:, :, ::-1]
         inputImage = inputImage.astype(np.float32)
